d180e79c566fc44671d30913a3912e35d26e773e
kemes <mikael.hirn@gmail.com>
2025-11-03 13:57:50 +0200

    Initial nvim config.

---
+++ nvim/init.lua
@@ -0,0 +1,28 @@
+vim.g.mapleader = ","
+vim.g.maplocalleader = "\\"
+
+require("config.lazy")
+
+local opts = { noremap = true, silent = true }
+
+local builtin = require('telescope.builtin')
+vim.keymap.set('i', '<leader>w', '<ESC>:w<CR>', opts)
+vim.keymap.set('i', '<C-h>', '<Left>', opts)
+vim.keymap.set('i', '<C-l>', '<Right>', opts)
+vim.keymap.set('i', '<C-j>', '<Down>', opts)
+vim.keymap.set('i', '<C-k>', '<Up>', opts)
+vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
+
+vim.cmd("set shiftwidth=4")
+vim.cmd("set tabstop=4")
+vim.cmd("set softtabstop=4")
+vim.cmd("set relativenumber")
+vim.cmd("set nowrap")
+vim.cmd("set scrolloff=99999999")
+vim.cmd("set splitbelow")
+vim.cmd("set splitright")
+vim.cmd("set wildmenu")
+vim.cmd("set nocompatible")
+vim.cmd("set hidden")
+
+vim.cmd("colorscheme catppuccin")
---
+++ nvim/lua/plugins/catppuccin.lua
@@ -0,0 +1,6 @@
+return {
+	"catppuccin/nvim",
+	lazy = false,
+	name = "catpputccin",
+	priority = 1000
+}
